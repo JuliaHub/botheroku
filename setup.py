c4974b4ee7f5b513fe3b8c415dbf17db76bd8278
Yulia Pashneva <pashneva@advert.local>
2018-12-10 13:17:41 +0200

    3

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
 import telebot
 from telebot.types import Message
+import os 
 
-
-TOKEN = '702156751:AAEBEQyrkY-wwvOuEileZ02CxmfMKG4RHCU'
+#TOKEN = '702156751:AAEBEQyrkY-wwvOuEileZ02CxmfMKG4RHCU'
+TOKEN = os.environ['TOKEN']
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['help'])
 def command_handler(message: Message):
-    bot.reply_to(message, 'help command helpi')  
+    bot.reply_to(message, 'help command helpi token heroku')  
 
 @bot.message_handler(content_types=['text'])
 def message_handler(message: Message):
-    bot.reply_to(message, 'test reply newwwwwww')
+    bot.reply_to(message, 'test reply newwwwwww  token heroku')
 
   
 bot.polling()
