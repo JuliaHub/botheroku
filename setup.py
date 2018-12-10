398ae744a653115fe7fe9626f83998b02ec19180
Yulia Pashneva <pashneva@advert.local>
2018-12-10 17:21:38 +0200

    5

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['help'])
 def command_handler(message: Message):
-    bot.reply_to(message, 'help command helpi token heroku')  
+    bot.reply_to(message, 'help command helpi token heroku config var')  
 
 @bot.message_handler(content_types=['text'])
 def message_handler(message: Message):
-    bot.reply_to(message, 'test reply newwwwwww  token heroku')
+    bot.reply_to(message, 'test reply newwwwwww  token heroku config var')
 
   
 bot.polling()
